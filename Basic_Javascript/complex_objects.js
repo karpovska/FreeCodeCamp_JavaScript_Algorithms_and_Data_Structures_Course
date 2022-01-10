b00931882ae9a974601638dd02057e3cc7830d19
Oksana Karpova <oksanachv@gmail.com>
2022-01-10 12:51:43 +0100

    Change directory for files

---
+++ Basic_Javascript/complex_objects.js
@@ -0,0 +1,14 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  myMusic.push({"artist": "Pink", "title": "So what", "release_year": 2020, "formats": ["CD", "mp3"]});
