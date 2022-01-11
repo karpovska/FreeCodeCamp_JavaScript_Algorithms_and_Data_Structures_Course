d4a4f3800be56d47d361df9a751a0889bf9722a0
Oksana Karpova <oksanachv@gmail.com>
2022-01-11 10:55:16 +0100

    Duplicated files deleted

--- hasOwnProperty.js
+++
@@ -1,9 +0,0 @@
-function checkObj(obj, checkProp) {
-    if(obj.hasOwnProperty(checkProp))
-        console.log(obj[checkProp]);
-    else
-        console.log("Not found");
-    console.log("finished");
-}
-checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift");
-console.log("finished");
