d2b1b51b885e243e4ee9379352e1259b653eeaea
Oksana Karpova <oksanachv@gmail.com>
2022-01-17 12:16:55 +0100

    Check array of objects to find if properties exist and hava proper value

---
+++ Basic_Javascript/profile_lookup.js
@@ -0,0 +1,57 @@
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+    let result = "";
+    for (let i = 0; i < contacts.length; i++) {
+        //console.log(contacts[i]);
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                console.log(contacts[i][prop]);
+                return contacts[i][prop];
+            } else {
+                result = "No such property";
+                console.log(result);
+                return result;
+            }
+        } else {
+            //console.log(contacts[i]);
+            result = "No such contact";
+        }        
+    }   
+    console.log(result);        
+    return result; 
+    // Only change code above this line
+  }
+  
+  lookUpProfile("Akira", "likes");
+  lookUpProfile("Kristia", "lastName"); 
+  lookUpProfile("Sherlock", "likes");
+  lookUpProfile("Harry", "likes");
+  lookUpProfile("Kristian", "lik");
