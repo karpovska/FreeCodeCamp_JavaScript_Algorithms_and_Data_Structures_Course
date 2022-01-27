f1ad7db20242ecc6348b7ed448afbfd73cb4de2a
Oksana Karpova <oksanachv@gmail.com>
2022-01-27 15:17:46 +0100

    Using desctructing assignment to assign variables from nested objects

--- ES6/destructing_assignments.js
+++ ES6/destructing_assignments.js
@@ -2,12 +2,16 @@ const HIGH_TEMPERATURES = {
     yesterday: 75,
     today: 77,
     tomorrow: 80
-  };
-  
-  // Only change code below this line
+  };  
   
   const { today, tomorrow } = HIGH_TEMPERATURES;
   const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
 
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };  
+    
+  const { today: { low: lowToday, high: highToday }} = LOCAL_FORECAST; 
   
-  // Only change code above this line
