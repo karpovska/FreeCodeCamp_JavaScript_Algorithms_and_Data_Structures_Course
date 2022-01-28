bccf1a50fff429a8f3b72d07bb4c74a57f4fb102
Oksana Karpova <oksanachv@gmail.com>
2022-01-28 12:48:40 +0100

    Using destructing assignment with rest parameter to reassing array elements

--- ES6/destructing_assignments.js
+++ ES6/destructing_assignments.js
@@ -18,4 +18,13 @@ const HIGH_TEMPERATURES = {
   let a = 8, b = 6;
   [a, b] = [b, a];
   console.log(`a: ${a}, b: ${b}`);
+
+  //Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() 
+  //so that arr is a sub-array of the original array source with the first two elements omitted.
+  const source = [1,2,3,4,5,6,7,8,9,10];
+  function removeFirstTwo(list) {    
+    const [, , ...arr] = list;     
+    return arr;
+  }
+  const arr = removeFirstTwo(source);
   
