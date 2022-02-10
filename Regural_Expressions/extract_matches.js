d7fbd2c382174d7def06388e49b4943afeeddb67
Oksana Karpova <oksanachv@gmail.com>
2022-02-10 12:49:03 +0100

    Using wildcard period to match any symbol

--- Regural_Expressions/extract_matches.js
+++ Regural_Expressions/extract_matches.js
@@ -7,3 +7,8 @@ let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/gi; 
 let secondResult = twinkleStar.match(starRegex); 
 console.log(secondResult);
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; 
+let thirdResult = unRegex.test(exampleStr);
+console.log(thirdResult);
