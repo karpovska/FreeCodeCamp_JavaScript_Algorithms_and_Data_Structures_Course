3a73695c7497f88185ba3445aca630e9f23995aa
Oksana Karpova <oksanachv@gmail.com>
2022-03-22 12:27:34 +0100

    Simple password check with positive lookahead

---
+++ Regural_Expressions/lookaheads.js
@@ -0,0 +1,5 @@
+//Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6,})(?=\D*\d\d)/; // Change this line
+let result = pwRegex.test(sampleWord);
