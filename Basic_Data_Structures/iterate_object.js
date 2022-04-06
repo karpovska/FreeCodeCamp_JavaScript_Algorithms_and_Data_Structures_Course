c2e7f805dba1ebf817be06c01c78aa3524855931
Oksana Karpova <oksanachv@gmail.com>
2022-04-06 13:37:06 +0200

    Iterate through objects with for.. in statement and evaluate objest that a passed as arguments

---
+++ Basic_Data_Structures/iterate_object.js
@@ -0,0 +1,24 @@
+const users = {
+    Alan: {
+      online: false
+    },
+    Jeff: {
+      online: true
+    },
+    Sarah: {
+      online: false
+    }
+  }
+  
+  function countOnline(usersObj) {
+    // Only change code below this line
+    let counter = 0;
+    for (let user in usersObj) {    
+      if(usersObj[user].online)
+        counter++;
+    }
+    return counter;
+    // Only change code above this line
+  }
+  
+  console.log(countOnline(users));
