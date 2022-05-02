beef49c069d19dffdef3eccdce9e375f01fb76a0
Oksana Karpova <oksanachv@gmail.com>
2022-05-02 21:29:34 +0200

    Using an IIFE to create a module

--- Object_Oriented_Programming/IIFE.js
+++ Object_Oriented_Programming/IIFE.js
@@ -2,4 +2,18 @@
     console.log("A cozy nest is ready");
   })();
   
+  let funModule = (function () {
+    return {
+      isCuteMixin: function(obj) {
+        obj.isCute = function() {
+          return true;
+        }
+      },
+      singMixin: function(obj) {
+        obj.sing = function() {
+          console.log("Singing to an awesome tune");
+        }
+      }
+    }
+  })();
   
