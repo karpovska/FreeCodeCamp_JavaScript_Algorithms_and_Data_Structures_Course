176dc55b3a2c693b8e6336eb8d282036d1180ee7
Oksana Karpova <oksanachv@gmail.com>
2022-05-10 11:05:02 +0200

    Refactor the code so it doesn't use global variables and don't change them

---
+++ Functional_Programming/books_refactor.js
@@ -0,0 +1,28 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(arr, bookName) {
+  let newArr = [...arr];
+  newArr.push(bookName);
+  return newArr;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(arr, bookName) {
+    let newArr = [...arr];
+    const book_index = arr.indexOf(bookName);
+  
+  if (book_index >= 0) {
+
+    newArr.splice(book_index, 1);
+    return newArr;
+
+    // Change code above this line
+    }
+}
+
+console.log(add(bookList, "A Brief History of Time"));
+console.log(remove(bookList, "On The Electrodynamics of Moving Bodies"));
