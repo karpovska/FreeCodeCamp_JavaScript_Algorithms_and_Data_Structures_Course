f92a7ee282d2756cf4528f211cd72fa125f58cee
Oksana Karpova <oksanachv@gmail.com>
2022-06-27 14:47:47 +0200

    Look though array of objects and find all objects that meet certain criteria

---
+++ Intermediate_Algorithm_Scripting/object_array.js
@@ -0,0 +1,12 @@
+/*Make a function that looks through an array of objects (first argument) and returns an array of all objects 
+that have matching name and value pairs (second argument). 
+Each name and value pair of the source object has to be present in the object from the collection 
+if it is to be included in the returned array. */
+
+function whatIsInAName(collection, source) {
+    const sourceKeys = Object.keys(source);    
+    return collection.filter(obj => sourceKeys.every(key => obj.hasOwnProperty(key) && obj[key] === source[key]));      
+                            
+  }
+  
+  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
