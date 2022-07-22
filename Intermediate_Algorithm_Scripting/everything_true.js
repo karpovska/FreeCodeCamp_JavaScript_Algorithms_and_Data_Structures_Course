d9344e6c30369efdc94f7ad24e4816b1f5ba628f
Oksana Karpova <oksanachv@gmail.com>
2022-07-22 06:11:38 +0200

    Check if all elements in collection of objects are true

---
+++ Intermediate_Algorithm_Scripting/everything_true.js
@@ -0,0 +1,19 @@
+/* Everything Be True
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context. */
+
+function truthCheck(collection, pre) {
+    let truthy = true;
+    collection.forEach(element => {
+      if(!element[pre]){        
+        truthy = false;
+      }    
+    }) 
+    return truthy; 
+  }
+  
+  console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+  console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name"));
