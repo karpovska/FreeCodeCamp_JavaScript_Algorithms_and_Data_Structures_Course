fb55fc8bab1490c7057019d3281500d4dd178f81
Oksana Karpova <oksanachv@gmail.com>
2022-08-12 22:09:54 +0200

    Find the orbital period of the object

---
+++ Intermediate_Algorithm_Scripting/orbital_period.js
@@ -0,0 +1,31 @@
+/*According to Kepler's Third Law, the orbital period  T  of two point masses orbiting each other in a circular or elliptic orbit is:
+
+T=2πa3μ−−−√
+ 
+a  is the orbit's semi-major axis
+μ=GM  is the standard gravitational parameter
+G  is the gravitational constant,
+M  is the mass of the more massive body.
+Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2. 
+*/
+
+function orbitalPeriod(arr) {
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+    arr.forEach(element => {
+      element.orbitalPeriod = Math.round(Math.sqrt( Math.pow(element.avgAlt + earthRadius, 3) / GM) * 2 * Math.PI);
+      delete element.avgAlt;
+    })
+    
+    return arr;
+  }
+  
+  console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
+  console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]));
+  
